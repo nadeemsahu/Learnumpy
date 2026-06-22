7de6622110270a92a10aa6b549fa005b7af7e269
nadeemsahu <nadeemsahun@gmail.com>
2026-06-23 00:13:14 +0530

    transpose

---
+++ 3.numpy axis/2.py
@@ -0,0 +1,9 @@
+#arr.T function mean transpose , 
+# shift rows to coloumns and coloumns to rows
+
+import numpy as np
+x=[[1,2,3],[4,5,6],[7,1,0]]
+arr=np.array(x)
+print(arr)
+print("\n")
+print(arr.T)
